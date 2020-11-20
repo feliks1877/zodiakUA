98f4511054015ebe0baf5a50efa384ec546bd857
feliks <feliksd1877@gmail.com>
2020-11-20 23:39:53 +0330

    style

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ async function start(){
             useFindAndModify: false
         })
         app.listen(PORT, () => {
-            console.log('Server is running on port',PORT)
+            console.log(`Server is running on port ${PORT}`)
         })
     }catch (e){
         console.log(e)
