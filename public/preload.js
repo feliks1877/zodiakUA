7fcb6aaf7293fee0526059481e0fe014ae99c86a
feliks1877 <64462047+feliks1877@users.noreply.github.com>
2020-11-27 15:20:30 +0330

    save

--- public/preload.js
+++ public/preload.js
@@ -1,23 +1,22 @@
-// export const preload = window.setTimeout( () => {
-//     let elem = document.createElement('div')
-//     elem.classList.add('preload')
-//     let img = new Image()
-//     img.classList.add('logoMy')
-//     img.src = "/images/LOGO.svg"
-//     elem.appendChild(img)
-//     console.log('comen')
-//     document.querySelector('html').insertAdjacentElement('afterbegin', elem)
-//     anime({
-//         targets: elem.children,
-//         scale: 0.7,
-//         opacity: 0.5,
-//         direction: 'alternate',
-//         loop: true,
-//         easing: 'cubicBezier(.5, .05, .1, .3)'
-//     });
-//     window.onload = () => {
-//         window.setTimeout(function () {
-//             elem.remove()
-//         }, 1000);
-//     }
-// },0)
+export const preload = window.setTimeout( () => {
+    let elem = document.createElement('div')
+    elem.classList.add('preload')
+    let img = new Image()
+    img.classList.add('logoMy')
+    img.src = "/images/LOGO.svg"
+    elem.appendChild(img)
+    document.querySelector('html').insertAdjacentElement('afterbegin', elem)
+    anime({
+        targets: elem.children,
+        scale: 0.7,
+        opacity: 0.5,
+        direction: 'alternate',
+        loop: true,
+        easing: 'cubicBezier(.5, .05, .1, .3)'
+    });
+    window.onload = () => {
+        window.setTimeout(function () {
+            elem.remove()
+        }, 1000);
+    }
+},0)
