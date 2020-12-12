fe49476a476e128e0b5b6e431784a9205e1af559
feliks1877 <64462047+feliks1877@users.noreply.github.com>
2020-12-12 13:09:11 +0330

    asde

--- public/script.js
+++ public/script.js
@@ -281,4 +281,31 @@ if (imgParallax != null) {
         imgParallax.setAttribute('src', '/images/5.jpg')
     }
 }
+////////////////// СКРЫТЬ НЕ ВИДИМЫЕ ИЗОБРАЖЕНИЯ /////////////////
+function isVisible(elem) {
+
+    let coords = elem.getBoundingClientRect();
+
+    let windowHeight = document.documentElement.clientHeight;
+
+    // верхний край элемента виден?
+    let topVisible = coords.top > 0 && coords.top < windowHeight;
+    // нижний край элемента виден?
+    let bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+    console.log(bottomVisible,topVisible,windowHeight)
+    return topVisible || bottomVisible;
+}
+function showVisible() {
+    for (let img of document.querySelectorAll('img')) {
+        let realSrc = img.dataset.src;
+        if (!realSrc) continue;
+        if (isVisible(img)) {
+            img.src = realSrc;
+            img.dataset.src = '';
+        }
+    }
+}
+showVisible();
+window.onscroll = showVisible;
+
 
